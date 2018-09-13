812ae85030e362eadb7a0162af69106645d9ce0a
Karen Chung-Yen Tsai <ktsai@lanl.gov>
2018-09-13 14:15:54 -0600

    removes data-science-no-stages pipeline

--- pipelines/data-science-no-stages/scripts/compare-output.py
+++
@@ -1,6 +0,0 @@
-from filecmp import cmp
-
-fnames = ['results/naive_bayes', 'results/svm_estimator']
-for f in fnames:
-    true_or_false = str(cmp(f+'_original.png', f+'.png')).lower()
-    print('[{}] {} is equal.'.format(true_or_false, f))
